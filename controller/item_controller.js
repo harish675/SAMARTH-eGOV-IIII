5fb0e742ec3fdc03b174d7750d4e5a3022949acb
Harish Jadhav <harishjadhav675@gmail.com>
2024-01-15 01:27:25 +0530

    create api for deleting item

--- controller/item_controller.js
+++ controller/item_controller.js
@@ -1,5 +1,6 @@
 
  const Item = require('../model/ItemDetails');
+
 // 1.create or add the product
 module.exports.addItem = async function(req,res){
    
@@ -23,11 +24,47 @@ module.exports.addItem = async function(req,res){
               message:"Internal Server Error"
          })
       }
-
 }
 
 //2.delete the product 
 module.exports.deleteItem = async function(req,res){
+
+   try{
+
+        const itemId = req.params.id;
+        console.log(itemId);
+
+        const deletedItem = await Item.findByIdAndDelete(itemId);
+
+        if(!deletedItem){
+          
+           console.log("Item not found");
+
+           return res.status(404).json({
+            
+               error:"Item not found"
+
+           })
+
+        }
+       
+        console.log("item is deleted Successfully", deletedItem);
+
+        return res.status(200).json({
+            message:"Item deleted successfully",
+            data:deletedItem
+        })  
+
+   }
+   catch(err){
+      
+       console.log("Error in deleting the Item" , err);
+
+       return res.status(500).json({
+          message:"Internal Server Error",
+       })
+
+   }
      
 
 }
